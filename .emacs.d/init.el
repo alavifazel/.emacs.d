3243c9450f59196bac1f2c9e8c5709801d51e0a3
Iman Alavi Fazel <im.alavifazel@gmail.com>
2018-08-31 05:15:40 -0400

    added autocomplete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,3 +32,7 @@
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
