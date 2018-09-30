f80556821f3c52a0c45ff21f3d37606efc92a8e2
Iman Alavi Fazel <im.alavifazel@gmail.com>
2018-09-30 03:12:19 -0400

    added CuaMode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,3 +45,11 @@
                     :width 'normal)
 ;; Loading a dark theme
 (load-theme 'deeper-blue) 
+
+
+;; CuaMode
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+
