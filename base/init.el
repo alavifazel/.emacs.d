a4035e8717a1df6147541d862e09199419614477
Iman Alavi Fazel <im.alavifazel@gmail.com>
2021-09-22 10:15:12 +0330

    Refractor IJava functions

--- base/init.el
+++ base/init.el
@@ -18,39 +18,12 @@
 (load-theme 'wombat)
 (show-paren-mode 1)
 
-;; Move them to Java major mode
-(defun sti ()
-  "Convert String to Integer."
+(defun load-scripts (file)
   (interactive)
-  (insert "Integer.parseInt();")
-  (backward-char 2))
+  (load-file (expand-file-name file "./iscripts/")))
 
-(defun its ()
-  "Convert Integer to String."
-  (interactive)
-  (insert "String.valueOf();")
-  (backward-char 2))
+(defun add-ijava ()
+    (load-scripts "java.el") )
 
-(transient-mark-mode 1)
-(defun gs ()
-  "Generate getters/setter."
-  (interactive)
-  (setq region-str (buffer-substring-no-properties (region-beginning) (region-end)))
-  (save-match-data
-    (and (string-match ".*\\([[:space:]].*[^[:space:]]\\)[[:space:]]*\\([[:space:]].*\\);$" region-str)
-	 (setq gs-type-p (match-string 1 region-str)
-	       gs-var-p (match-string 2 region-str))))
-  (setq gs-type (substring-no-properties gs-type-p 1 nil))
-  (setq gs-var (substring-no-properties gs-var-p 1 nil))
-  (print gs-type)
-  (goto-char (region-end))
+(advice-add 'java-mode :before #'add-ijava)
 
-  (insert "\n\n")
-  (insert (concat "public " gs-type " " "get" (capitalize gs-var) "(" gs-type " " gs-var ") { \n" ))
-  (insert (concat "\t" "return this." gs-var ";\n"))
-  (insert "}\n")
-
-  (insert "\n")
-  (insert (concat "public void set" (capitalize gs-var) "(" gs-type " " gs-var ") { \n" ))
-  (insert (concat "\t" "this." gs-var " = " gs-var ";\n"))
-  (insert "}\n") )
---
+++ base/iscripts/java.el
@@ -0,0 +1,37 @@
+(defun sti ()
+  "Convert String to Integer."
+  (when (eq major-mode 'java-mode))
+  (interactive)
+  (insert "Integer.parseInt();")
+  (backward-char 2))
+
+(defun its ()
+  "Convert Integer to String."
+  (when (eq major-mode 'java-mode))
+  (interactive)
+  (insert "String.valueOf();")
+  (backward-char 2))
+
+(defun gs ()
+  "Generate getters/setter."
+  (when (eq major-mode 'java-mode))
+  (interactive)
+  (setq region-str (buffer-substring-no-properties (region-beginning) (region-end)))
+  (save-match-data
+    (and (string-match ".*\\([[:space:]].*[^[:space:]]\\)[[:space:]]*\\([[:space:]].*\\);$" region-str)
+	 (setq gs-type-p (match-string 1 region-str)
+	       gs-var-p (match-string 2 region-str))))
+  (setq gs-type (substring-no-properties gs-type-p 1 nil))
+  (setq gs-var (substring-no-properties gs-var-p 1 nil))
+  (print gs-type)
+  (goto-char (region-end))
+
+  (insert "\n\n")
+  (insert (concat "public " gs-type " " "get" (capitalize gs-var) "(" gs-type " " gs-var ") { \n" ))
+  (insert (concat "\t" "return this." gs-var ";\n"))
+  (insert "}\n")
+
+  (insert "\n")
+  (insert (concat "public void set" (capitalize gs-var) "(" gs-type " " gs-var ") { \n" ))
+  (insert (concat "\t" "this." gs-var " = " gs-var ";\n"))
+  (insert "}\n") )
