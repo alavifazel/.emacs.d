b494fc89349b5b81ef3b3d33af0a9f27afba3ae3
Iman Alavi Fazel <im.alavifazel@gmail.com>
2021-09-22 04:13:11 +0330

    Update

--- base/init.el
+++ base/init.el
@@ -1,58 +1,56 @@
-(setq user-init-file (or load-file-name (buffer-file-name)))
-(setq user-emacs-directory (file-name-directory user-init-file))
-  
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(cua-mode t)
-(setq cua-auto-tabify-rectangles nil)
-(transient-mark-mode 1)
-;; (setq cua-keep-region-after-copy t)
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-(global-linum-mode t) 
-
-(global-set-key "\M-k" '(lambda () (interactive) (kill-line 0)) ) ; defines M-k as Backward C-k
-
-(global-set-key (kbd "C-w") 'backward-kill-word) ; C-w deletes previous word; just like terminal
-
-(electric-pair-mode 1)
-
-(global-set-key (kbd "C-s") 'save-buffer)
-(global-unset-key (kbd "C-x C-s"))
-(global-unset-key (kbd "C-x s"))
-
-(global-set-key (kbd "C-f") 'isearch-forward)
-
-(global-set-key (kbd "C-q") 'save-buffers-kill-emacs)
-(global-unset-key (kbd "C-x C-c"))
-
-(global-set-key (kbd "C-o") 'find-file)
-(global-unset-key (kbd "C-x C-f"))
-
-(global-set-key (kbd "C-z") 'undo)
-
-;; packages
-
-;; NEOTREE
-;; $ mkdir ~/.emacs.d/packages/ || cd "$_"
-;; $ git clone https://github.com/jaypei/emacs-neotree.git neotree
-;; $ cd neotree
-;; $ git checkout dev
-
-(add-to-list 'load-path "~/.emacs.d/packages/neotree")
-(require 'neotree)
-(global-set-key [f2] 'neotree-toggle)
+(require 'evil)
+(evil-mode 1)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (company evil))))
+(custom-set-faces)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(electric-pair-mode t)
+(load-theme 'wombat)
+(show-paren-mode 1)
+
+;; Move them to Java major mode
+(defun sti ()
+  "Convert String to Integer."
+  (interactive)
+  (insert "Integer.parseInt();")
+  (backward-char 2))
+
+(defun its ()
+  "Convert Integer to String."
+  (interactive)
+  (insert "String.valueOf();")
+  (backward-char 2))
+
+(transient-mark-mode 1)
+(defun gs ()
+  "Generate getters/setter."
+  (interactive)
+  (setq region-str (buffer-substring-no-properties (region-beginning) (region-end)))
+  (save-match-data
+    (and (string-match ".*\\([[:space:]].*[^[:space:]]\\)[[:space:]]*\\([[:space:]].*\\);$" region-str)
+	 (setq gs-type-p (match-string 1 region-str)
+	       gs-var-p (match-string 2 region-str))))
+  (setq gs-type (substring-no-properties gs-type-p 1 nil))
+  (setq gs-var (substring-no-properties gs-var-p 1 nil))
+  (print gs-type)
+  (goto-char (region-end))
+
+  (insert "\n\n")
+  (insert (concat "public " gs-type " " "get" (capitalize gs-var) "(" gs-type " " gs-var ") { \n" ))
+  (insert (concat "\t" "return this." gs-var ";\n"))
+  (insert "}\n")
+
+  (insert "\n")
+  (insert (concat "public void set" (capitalize gs-var) "(" gs-type " " gs-var ") { \n" ))
+  (insert (concat "\t" "this." gs-var " = " gs-var ";\n"))
+  (insert "}\n") )
