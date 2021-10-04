29de50e42332012aba3b26c6f4ae09c20f77b134
Iman Alavi Fazel <im.alavifazel@gmail.com>
2021-10-04 23:51:06 +0330

    Add smooth scrolling

--- base/init.el
+++ base/init.el
@@ -34,3 +34,32 @@
   (interactive)
   (shell-command terminal-name) )
 
+(defun files()
+(defconst files-name "nautilus")
+  (interactive)
+  (shell-command files-name) )
+
+(setq doc-view-continuous t)
+(setq doc-view-resolution 200)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+    
+(setq mouse-wheel-scroll-amount '(3 ((shift) . 3)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-step 2)
+
+(display-line-numbers-mode)
+(load-theme 'tsdh-light)
+
+(global-tab-line-mode t)
+(setq tab-line-new-button-show nil)
+(setq tab-line-close-button-show nil)
+(setq tab-line-separator "")
+
+(display-line-numbers-mode)
+(setq redisplay-dont-pause t
+  scroll-margin 1
+  scroll-step 1
+  scroll-conservatively 10000
+  scroll-preserve-screen-position 1)
