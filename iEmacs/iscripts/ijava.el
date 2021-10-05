c2f008a28050de3aed6b1effd06e0df61c5d23b1
Iman Alavi Fazel <im.alavifazel@gmail.com>
2021-10-05 22:29:52 +0330

    Fix IJava

--- iEmacs/iscripts/ijava.el
+++ iEmacs/iscripts/ijava.el
@@ -107,6 +107,8 @@
     )
   )
 
+;; Makefile generation
+
 (global-set-key [f8] 'makefile)
 
 (defun write-makefile ()
@@ -117,33 +119,3 @@
     )
 
 (call-interactively 'gen-makefile ())
-(setq scroll-conservatively 10000)(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-(require 'evil)
-(evil-mode 1)
-(custom-set-variables
- '(inhibit-startup-screen t)
- '(package-selected-packages (quote (company evil))))
-(custom-set-faces)
-(add-hook 'after-init-hook 'global-company-mode)
-
-(electric-pair-mode t)
-(load-theme 'wombat)
-(show-paren-mode 1)
-
-(defun load-scripts (file)
-  (interactive)
-  (load-file (expand-file-name file "~/.emacs.d/iscripts/")))
-
-(defun add-ijava ()
-    (load-scripts "ijava.el") )
-
-(advice-add 'java-mode :before #'add-ijava)
-
-(setq scroll-conservatively 1000)
