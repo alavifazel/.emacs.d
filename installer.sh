b7babb0536b3817c4d078d0eeb2ec4e147d11246
Iman Alavi Fazel <im.alavifazel@gmail.com>
2021-12-07 18:32:02 +0330

    Update

--- installer.sh
+++ installer.sh
@@ -2,9 +2,9 @@
 
 echo "Applying the config..."
 
-cp ./iEmacs/init.el ~/.emacs.d/
-cp -r ./iEmacs/iscript ~/.emacs.d/
-cp -r ./iEmacs/config ~/.emacs.d/
+cp ./init.el ~/.emacs.d/
+cp -r ./iscript ~/.emacs.d/
+cp -r ./config ~/.emacs.d/
 
 while true; do
     read -p "Do you wish to add the alias 'e' for the emacs? (yes/no)" yn
